ba18efebca400f3b2ca1597d5f723aab1cf670fd
Evgeniy Antonov <zhekahead@gmail.com>
2022-06-21 16:38:27 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+"use strict";
+
+let message;
+let login;
+
+login == "Сотрудник" ? message = "Привет" :
+login == "Директор" ? message = "Здравствуйте" :
+login == "" ? message = "Нет логина" : message = "";
